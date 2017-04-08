3530df79ee442a8fe925ea2009e92125c5fccc19
alfredlau <jian.liu@icloud.com>
2017-04-08 12:56:59 +0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+### vue-dashboard
+
+用来对 [vue_mywebsite](https://github.com/Alfred-Lau/my_website)统一进行后台管理和数据的录入,报表分析
+
---
+++ src/api/api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+let base = '';
+
+// 注意: 这里是前端的数据控制中心,从 vue 文件中抽离出来的 Ajax 数据访问方式, 而不是后端
+
+export const requestLogin = params => { return axios.post(`${base}/login`, params).then(res => res.data); };
+
+export const getUserList = params => { return axios.get(`${base}/user/list`, { params: params }); };
+
+export const getUserListPage = params => { return axios.get(`${base}/user/listpage`, { params: params }); };
+
+export const removeUser = params => { return axios.get(`${base}/user/remove`, { params: params }); };
+
+export const batchRemoveUser = params => { return axios.get(`${base}/user/batchremove`, { params: params }); };
+
+export const editUser = params => { return axios.get(`${base}/user/edit`, { params: params }); };
+
+export const addUser = params => { return axios.get(`${base}/user/add`, { params: params }); };
